a9fbd5ede9a7cc05fdca764ab15a86264fd68fe8
alexhstrickland <alexhstrickland@gmail.com>
2021-01-16 23:13:46 -0500

    Weather is displaying in the top container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
 $("document").ready(function(){
 
     var city;
+    var country;
     var cityName;
     var results;
     var currentTemp;
     var windSpeed;
     var humidity;
-    var UVIndex;
+    var UVIndexText;
+    var UVIndexColor;
     var iconW;
     var iconImage;
     var imageUrl;
+    var lat;
+    var lon;
     var currentDay = "(" + moment().format('l') + ")";
 
     $("#cityBtn").on("click", function(event) {
@@ -31,7 +35,7 @@ $("document").ready(function(){
             method: "GET"
         }).then(function(response) {
             console.log(response);
-            // getMoreInfo(response);
+            getMoreInfo(response);
         })
     }
 
@@ -39,19 +43,68 @@ $("document").ready(function(){
         var results =  response;
         var iconW = results.weather[0].icon;
         var imageUrl = "http://openweathermap.org/img/w/" + iconW + ".png";
+        console.log(imageUrl);
         var iconImage = $("<img>");
-        iconImage.attr("src", imageUrl)
-        var cityName = results.name + " " + currentDay + iconImage;
-        // var cityName = results.name + ", " + results.city.country + " " + currentDay;
+        iconImage.attr("src", imageUrl);
+        console.log(iconImage);
         var currentTemp = results.main.temp + " F";
         var humidity = results.main.humidity;
         var windSpeed = results.wind.speed + " MPH";
+        var lat = results.coord.lat;
+        var lon = results.coord.lon;
+        console.log(lat, lon);
         // var UVIndex = ;
-        console.log(cityName, currentTemp, humidity, windSpeed);
-        $("#cityName").append();
-        $("#currentTemp").append();
-        $("#humidity").append();
-        $("#windSpeed").append();
+    
+        var forecastURL= "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial" + "&appid=8391498daeaf403c89574dc9e5a777c7";
+
+        $.ajax({
+            url: forecastURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            
+        
+
+
+        var country = response.city.country;
+        var cityName = results.name + ", " + country + " " + currentDay;
+    
+        $("#cityName").append(cityName, iconImage);
+        $("#currentTemp").append("Temperature: " + currentTemp);
+        $("#humidity").append("Humidity: " + humidity);
+        $("#windSpeed").append("Wind Speed: " + windSpeed);
+
+
+            var UVURL= "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&units=imperial" + "&appid=8391498daeaf403c89574dc9e5a777c7";
+
+            $.ajax({
+                url: UVURL,
+                method: "GET"
+            }).then(function(response) {
+                console.log(response);
+
+                var UVIndexText = response.value;
+                var UVIndex = $("<button type='button'>");
+
+                // <button type="button" class="btn btn-success">Success</button>
+                // <button type="button" class="btn btn-danger">Danger</button>
+                // <button type="button" class="btn btn-warning">Warning</button>
+
+                if (parseInt(UVIndexText) < 3) {
+                    var UVIndexColor = "btn btn-success";
+                } else if (parseInt(UVIndexText) >= 3 && parseInt(UVIndexText) <= 5 ) {
+                    var UVIndexColor = "btn btn-warning";
+                } else if (parseInt(UVIndexText) > 5) {
+                    var UVIndexColor = "btn btn-danger";
+                }
+                UVIndex.addClass(UVIndexColor);
+
+                $("#UVIndex").append(("UV Index: "),(UVIndex.append(UVIndexText)));
+
+
+
+            })
+        })
     }
 
 
